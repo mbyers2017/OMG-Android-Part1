6e1b52bb7d768c2935f33c680ca8302b90d1fb4f
mbyers2017 <mbyers@hmc.edu>
2015-09-08 00:15:36 -0700

    Added text on the screen through the Java code.

--- app/src/main/java/maxbyers/omgandroid/MainActivity.java
+++ app/src/main/java/maxbyers/omgandroid/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1. Access the TextView defined in layout XML
+        // and then set its text
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
     }
 
     @Override
