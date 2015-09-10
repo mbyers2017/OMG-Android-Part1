9aeedf27628ed2099993cd1dfb4a5d789c2fa72a
mbyers2017 <mbyers@hmc.edu>
2015-09-09 20:30:44 -0700

    Mostly matches tutorial for adding names to a list.

--- app/src/main/java/maxbyers/omgandroid/MainActivity.java
+++ app/src/main/java/maxbyers/omgandroid/MainActivity.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
     EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // 3. Access the EditText defined in layout XML
         mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        // 4. Access the ListView
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                                        mNameList);
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
     }
 
     @Override
@@ -43,6 +59,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        // Also add that value to the list shown in the ListView
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+
         // Take what was typed into the EditText
         // and use in TextView
         mainEditText.setText(mainEditText.getText().toString()
